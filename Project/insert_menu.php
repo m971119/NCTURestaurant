5243607ebf0e50e83c9950a7fda45c8a5e8e5f65
LHIXY <sdfm2321791620@gmail.com>
2018-06-13 21:45:12 +0800

    0613
    新增菜單檔案insert_submit
    改Database_menu_mrid型態為int
    回首頁按鈕(新增、修改菜單都有)

--- Project/insert_menu.php
+++ Project/insert_menu.php
@@ -29,51 +29,29 @@ h3{
 
 <div>
 <?php echo"<h1>【 $rname 】 <label class=\"update\">菜單新增</label></h1>";?>
-	<form  onsubmit="return checkform()" action="update_submit.php" method="post" name="updateform">
+	<form  onsubmit="return checkform()" action="insert_submit.php" method="post" name="updateform">
 			
 		<?php 
 			echo"<h3>▶ 店家帳號： <span class=\"error\">$acc </span></h3>"; 
 		?>
-			<label class="word">。欲修改品項： </label><br>
+			<label class="word">。我想新增新菜色： </label><br>
+			<input type="text" name="new">
 			<form>
-			<select name="mid">
-		<?php 
-			$sql ="SELECT mid,mname,mprice,mintro,mveg,mcal FROM menu WHERE mrid=$erid";
-			$result = $conn->query($sql);
-			if($result->num_rows > 0){
-				//echo "<h2>拿到資料</h2>";
-				while($row = $result->fetch_row())
-				{//fetch_assoc():把傳出的資料分割好放入row這個array中
-					$mid = $row[0];
-					$mname = $row[1];
-					$mprice = $row[3];
-					$mintro = $row[4];
-					$mveg = $row[5];
-					$mcal = $row[6];
-					echo"
-					<option value=\"$mid\"> <p class=\"menu\">$mname</p> </option>
-					";
-				}
-			}
-			else{
-			  echo "<h2>資料庫連接失敗</h2>";
-			}			
-		?>
-			</select>
 			<br>
 		
-			<label class="word">。修改價格：</label>
+			<label class="word">。新菜菜的價格：</label>
 			<input type="number" name="mprice" min="0" max="10000">
 			<br>
-			<label class="word">。修改介紹：</label><br>
+			<label class="word">。新菜菜介紹：</label><br>
 			<input type="text" name="mintro">
 			<br>
-			<label class="word">。修改卡路里數(大卡)：</label><br>
+			<label class="word">。新菜菜的卡路里數(大卡)：</label><br>
 			<input type="number" name="mcal">
 			
 			<br>
-			<input type="submit" value="確認修改">
+			<input type="submit" value="新菜菜上市">
 	</form>
+	<a href="home.php"><button>回到首頁</button></a>
 </div>
 <script>
 	function checkform(){
---
+++ Project/insert_submit.php
@@ -0,0 +1,102 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="login_styles.css">
+<style>
+h3{
+	font-family:Bahnschrift SemiBold,Courier,微軟正黑體;
+	text-align:right;
+	margin-right:50px;
+}
+.word2{
+	font-family: "微軟正黑體";
+	font-size:35px;
+	color:1f3d7a;
+	text-align:left;
+	padding-left:80px;
+}
+.word3{
+	font-family: "微軟正黑體";
+	font-family: Courier;
+	color:6699ff;
+}
+
+</style>
+</head>
+
+<body style="background-color:black; margin: 50px 300px 50px 300px;">
+
+<div style="padding:40px;">
+
+<?php
+//REGISTER
+        include ("Project_connMySQL.php");
+		//記得啟動session		
+		session_start();
+        $eacc = $_SESSION['account'];
+		$erid = $_SESSION['erid'];
+		$new = $_POST['new'];//修改後的新菜名
+		$mprice = $_POST['mprice'];//修改後的價錢
+		$mintro = $_POST['mintro'];//修改後的簡介
+		$mcal = $_POST['mcal'];//修改後的卡路里
+		//取餐廳名稱
+		$sql ="SELECT rname FROM restaurant where rid='$erid'";
+		$result=$conn->query($sql);
+		$row = mysqli_fetch_assoc($result);
+		$rname = $row["rname"];
+		//取更新的菜名稱
+		//$sql ="SELECT mname FROM menu where mid='$mid'";
+		//$result=$conn->query($sql);
+		//$row = mysqli_fetch_assoc($result);
+		//$mname = $row["mname"];
+		
+		echo"
+			<h1>【 $rname 】 <label class=\"update\">新菜色增加成功！</label></h1>
+			<label class=\"mname\">• $new •</label>
+			";
+		
+		if(!(empty($mprice))&&(!(empty($mintro)))&&(!(empty($mintro)))){
+			$sql4 ="SELECT mid FROM menu where mrid='$erid'";
+			$result= $conn->query($sql4);
+			//$row = mysqli_fetch_assoc($result);
+			$rr = $result->num_rows;
+			$newnum = 100*$erid + $rr + 1;
+			//echo"$newnum";
+			
+			$sql1 = "INSERT INTO menu VALUES('$erid','$newnum','$new','$mprice','$mintro','0','$mcal')";
+			$result1 = $conn->query($sql1);
+			if($result1)
+				echo"
+			<label class=\"word2\"> <br>✔ 新菜菜價格： <span class=\"word3\">NT $mprice </span></label>
+			<label class=\"word2\"> <br>✔ 新菜菜介紹： <span class=\"word3\"> $mintro </span></label>
+			<label class=\"word2\"> <br>✔ 新菜菜卡路里： <span class=\"word3\"> $mcal 大卡</span></label>
+			";
+		}
+		/*if(!(empty($mintro))){
+			$sql2 = " menu SET mintro='$mintro' WHERE mid='$mid' ";
+			$result2 = $conn->query($sql2);
+			if($result2)
+				echo"
+			<label class=\"word2\"> <br>✔ 成功更新介紹： <span class=\"error\"> $mintro </span></label>
+			";
+		}
+		if(!(empty($mcal))){
+			$sql3 = " UPDATE menu SET mcal='$mcal' WHERE mid='$mid' ";
+			$result3 = $conn->query($sql3);
+			if($result3)
+				echo"
+			<label class=\"word2\"> <br>✔ 成功更新卡路里： <span class=\"error\"> $mcal 大卡</span></label>
+			";
+		}*/
+		
+		$link="home.php";
+		echo"
+		<br><br><a href='".$link."'><button>回到首頁</button></a>
+		";
+	?>
+
+
+</div>
+
+</body>
+</html>
